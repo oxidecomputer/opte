9d4a31bbde38137d3b78fcf15c3583e464fb440f
Ryan Zezeski <ryan@oxide.computer>
2022-02-02 14:10:05 -0500

    WIP add script for tracking gen_desc failures

---
+++ dtrace/opte-gen-desc-fail.d
@@ -0,0 +1,43 @@
+/*
+ * Track gen_desc() failures for stateful actions.
+ *
+ * dtrace -Cqs ./opte-gen-desc-fail.d
+ */
+typedef struct flow_id_sdt_arg {
+	uint32_t	src_ip;
+	uint32_t	dst_ip;
+	uint16_t	src_port;
+	uint16_t	dst_port;
+	uint8_t		proto;
+} flow_id_sdt_arg_t;
+
+BEGIN {
+	/*
+	 * Use an associative array to stringify the protocol number.
+	 */
+	protos[1] = "ICMP";
+	protos[2] = "IGMP";
+	protos[6] = "TCP";
+	protos[17] = "UDP";
+}
+
+gen-desc-fail {
+	this->flow_id = (flow_id_sdt_arg_t *)arg0;
+	this->msg = arg1;
+
+	/*
+	 * inet_ntoa() wants an ipaddr_t pointer, but opte is passing
+	 * up the actual 32-bit IP value. You can't take the address
+	 * of a dynamic variable, so make local allocations to
+	 * reference.
+	 */
+	this->srcp = (ipaddr_t *)alloca(4);
+	this->dstp = (ipaddr_t *)alloca(4);
+	*this->srcp = this->flow_id->src_ip;
+	*this->dstp = this->flow_id->dst_ip;
+
+	printf("%s %s:%u %s:%u %s\n", protos[this->flow_id->proto],
+	    inet_ntoa(this->srcp), this->flow_id->src_port,
+	    inet_ntoa(this->dstp), this->flow_id->dst_port,
+	    stringof(this->msg));
+}
