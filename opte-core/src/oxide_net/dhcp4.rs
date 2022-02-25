dea91b3fcca9fc6c2c4880735a69c0ba90ac0c35
Ryan Zezeski <ryan@oxide.computer>
2022-02-25 14:39:04 -0700

    XXX properly set UDP length and subnet mask

--- opte-core/src/oxide_net/dhcp4.rs
+++ opte-core/src/oxide_net/dhcp4.rs
@@ -34,8 +34,7 @@ use crate::ip4::{
 };
 use crate::layer::Layer;
 use crate::packet::{
-    Initialized, Packet, PacketMeta, PacketRead, PacketReader, PacketWriter,
-    Parsed
+    Initialized, Packet, PacketMeta, PacketRead, PacketReader, Parsed
 };
 use crate::port::{self, Port, Pos};
 use crate::rule::{
@@ -115,6 +114,15 @@ pub enum Dhcp4ReplyType {
     Ack,
 }
 
+impl Display for Dhcp4ReplyType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Offer => write!(f, "OFFER"),
+            Self::Ack => write!(f, "ACK"),
+        }
+    }
+}
+
 impl From<Dhcp4ReplyType> for DhcpMessageType {
     fn from(rt: Dhcp4ReplyType) -> Self {
         use smoltcp::wire::DhcpMessageType as SmolDMT;
@@ -128,7 +136,7 @@ impl From<Dhcp4ReplyType> for DhcpMessageType {
 
 impl Display for Dhcp4Action {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "DHCPv4: {}", self.guest_ip4)
+        write!(f, "DHCPv4 {}: {}", self.reply_type, self.guest_ip4)
     }
 }
 
@@ -140,9 +148,9 @@ impl HairpinAction for Dhcp4Action {
     ) -> GenResult<Packet<Initialized>> {
         let body = rdr.copy_remaining();
         // TODO Deal with failure.
-        let offer_pkt = DhcpPacket::new_checked(&body).unwrap();
+        let guest_pkt = DhcpPacket::new_checked(&body).unwrap();
         // TODO Deal with failure.
-        let offer = DhcpRepr::parse(&offer_pkt).unwrap();
+        let guest_dhcp = DhcpRepr::parse(&guest_pkt).unwrap();
 
         // TODO: Based on client's parameter request list need to add
         // certain DHCP options (or not).
@@ -150,13 +158,13 @@ impl HairpinAction for Dhcp4Action {
 
         let reply = DhcpRepr {
             message_type: mt.into(),
-            transaction_id: offer.transaction_id,
+            transaction_id: guest_dhcp.transaction_id,
             client_hardware_address: EthernetAddress::from(self.guest_mac),
             client_ip: Ipv4Address::UNSPECIFIED,
             your_ip: Ipv4Address::from(self.guest_ip4),
             server_ip: Ipv4Address::from(self.gw_ip4),
             router: Some(Ipv4Address::from(self.gw_ip4)),
-            subnet_mask: Some(Ipv4Address::from(self.subnet.to_mask())),
+            subnet_mask: Some(Ipv4Address::new(255, 255, 255, 255)),
             relay_agent_ip: Ipv4Address::UNSPECIFIED,
             broadcast: false,
             requested_ip: None,
@@ -200,7 +208,7 @@ impl HairpinAction for Dhcp4Action {
             src: 67,
             dst: 68,
         });
-        udp.set_pay_len(reply.buffer_len() as u16);
+        udp.set_pay_len(tmp.len() as u16);
 
         let mut ip4 = Ipv4Hdr::from(&Ipv4Meta {
             src: self.gw_ip4,
@@ -226,3 +234,90 @@ impl HairpinAction for Dhcp4Action {
     }
 }
 
+#[test]
+// Temorary test to make sure the reply can be parsed.
+fn tmp_parse() {
+    let mt = DhcpMessageType::from(Dhcp4ReplyType::Offer);
+    let guest_ip4 = Ipv4Addr::new([192, 168, 1, 245]);
+    let guest_mac = EtherAddr::from([0xa8, 0x40, 0x25, 0xfb, 0x8e, 0xa0]);
+    let gw_mac = EtherAddr::from([0xa8, 0x40, 0x25, 0x77, 0x77, 0x77]);
+    let gw_ip4 = Ipv4Addr::new([192, 168, 1, 1]);
+    let subnet = Ipv4Cidr::new(guest_ip4, 24).unwrap();
+
+    let reply = DhcpRepr {
+        message_type: mt.into(),
+        transaction_id: 7777,
+        client_hardware_address: EthernetAddress::from(guest_mac),
+        client_ip: Ipv4Address::UNSPECIFIED,
+        your_ip: Ipv4Address::from(guest_ip4),
+        server_ip: Ipv4Address::from(gw_ip4),
+        router: Some(Ipv4Address::from(gw_ip4)),
+        subnet_mask: Some(Ipv4Address::from(subnet.to_mask())),
+        relay_agent_ip: Ipv4Address::UNSPECIFIED,
+        broadcast: false,
+        requested_ip: None,
+        // TODO Look into this more: my guess is that if a client
+        // sends a client ID, then the server should response back
+        // with that ID.
+        client_identifier: None,
+        server_identifier: Some(Ipv4Address::from(gw_ip4)),
+        parameter_request_list: None,
+        // TODO fill this in, use some external resolver for now.
+        dns_servers: None,
+        max_size: None,
+        lease_duration: Some(86400),
+    };
+
+    let reply_len = reply.buffer_len();
+
+    let island_net = Ipv4Cidr::new(guest_ip4, 32).unwrap();
+    let def_route = Ipv4Cidr::new(ip4::ANY_ADDR, 0).unwrap();
+    let csr_opt = ClasslessStaticRouteOpt::new(
+        SubnetRouterPair::new(island_net, ip4::ANY_ADDR),
+        Some(SubnetRouterPair::new(def_route, gw_ip4)),
+        None,
+    );
+
+    // TODO This is temporary until I can add interface to Packet
+    // to initialize a zero'd mblk of N bytes and then get a
+    // direct mutable reference to the PacketSeg.
+    let mut tmp = vec![0u8; reply_len];
+    let mut dhcp = DhcpPacket::new_unchecked(&mut tmp);
+    let _ = reply.emit(&mut dhcp).unwrap();
+
+    // Need to overwrite the End Option with Classless Static
+    // Route Option and then write new End Option marker.
+    assert_eq!(tmp.pop(), Some(255));
+    tmp.extend_from_slice(&csr_opt.encode());
+    tmp.push(255);
+    assert_eq!(tmp.len(), reply_len + csr_opt.encode_len() as usize);
+
+    let mut udp = UdpHdr::from(&UdpMeta {
+        src: 67,
+        dst: 68,
+    });
+    udp.set_pay_len(tmp.len() as u16);
+
+    let mut ip4 = Ipv4Hdr::from(&Ipv4Meta {
+        src: gw_ip4,
+        dst: ip4::LOCAL_BROADCAST,
+        proto: Protocol::UDP,
+    });
+    ip4.set_total_len(ip4.hdr_len() as u16 + udp.total_len());
+    ip4.compute_hdr_csum();
+
+    let eth = EtherHdr::from(&EtherMeta {
+        dst: ether::ETHER_BROADCAST,
+        src: gw_mac,
+        ether_type: ether::ETHER_TYPE_IPV4,
+    });
+
+    let mut pkt_bytes = Vec::with_capacity(ETHER_HDR_SZ + IPV4_HDR_SZ +
+                                           UDP_HDR_SZ + tmp.len());
+    pkt_bytes.extend_from_slice(&eth.as_bytes());
+    pkt_bytes.extend_from_slice(&ip4.as_bytes());
+    pkt_bytes.extend_from_slice(&udp.as_bytes());
+    pkt_bytes.extend_from_slice(&tmp);
+    let pkt = Packet::copy(&pkt_bytes).parse().unwrap();
+    println!("pkt: {:#?}", pkt);
+}
