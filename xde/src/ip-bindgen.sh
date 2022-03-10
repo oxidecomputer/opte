670c3638e3588743a19640b11b8070cb2af7a4dc
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-03-10 10:54:05 -0800

    add bindgen script for generating ip.rs

---
+++ xde/src/ip-bindgen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$ILLUMOS_GATE" ]]; then
+    echo "Must set ILLUMOS_GATE environment variable to gate source dir"
+    exit 1
+fi
+
+export LD_LIBRARY_PATH=/opt/ooce/clang-12.0/lib/ 
+
+bindgen ip.h \
+    --no-layout-tests \
+    --ctypes-prefix=illumos_ddi_dki \
+    --use-core \
+    -- \
+    -I$ILLUMOS_GATE/usr/src/uts/common \
+    -D_KERNEL \
+    > ip.rs
