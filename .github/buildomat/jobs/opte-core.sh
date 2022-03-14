8faec4ae98f5a2812755204fca37e01eb37f8e82
Ryan Zezeski <ryan@oxide.computer>
2022-03-14 00:45:27 -0600

    XXX why are you so needy usdt?

--- .github/buildomat/jobs/opte-core.sh
+++ .github/buildomat/jobs/opte-core.sh
@@ -20,26 +20,32 @@ rustc --version
 
 cd opte-core
 
+#
+# XXX usdt has been giving me all sorts of grief, so for now do not
+# enable that feature:
+#
+#    --no-default-features --features std
+#
 header "check style"
 ptime -m cargo +nightly fmt -- --check
 
 header "analyze"
-ptime -m cargo +nightly check
+ptime -m cargo +nightly check --no-default-features --features std
 
 header "analyze no_std"
 ptime -m cargo +nightly check --no-default-features
 
 header "debug build"
-ptime -m cargo +nightly build
+ptime -m cargo +nightly build --no-default-features --features std
 
 header "debug build no_std"
 ptime -m cargo +nightly build --no-default-features
 
 header "release build"
-ptime -m cargo +nightly build --release
+ptime -m cargo +nightly build --release --no-default-features --features std
 
 header "release build no_std"
 ptime -m cargo +nightly build --release --no-default-features
 
 header "test"
-ptime -m cargo +nightly test
+ptime -m cargo +nightly test --no-default-features --features std
