b5ed9c388173d7f1e6c425f4c46161674e9d5db2
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-03-30 19:26:57 -0700

    P5p packaging (#86)
    This PR packages up the following into a P5P package
    - xde driver
    - opteadm cli tool

---
+++ .github/buildomat/jobs/p5p.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#:
+#: name = "opte-p5p"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "nightly"
+#: output_rules = [
+#:   "/out/*",
+#: ]
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+#
+# TGT_BASE allows one to run this more easily in their local
+# environment:
+#
+#   TGT_BASE=/var/tmp ./xde.sh
+#
+TGT_BASE=${TGT_BASE:=/work}
+
+REL_SRC=target/x86_64-unknown-unknown/release
+REL_TGT=$TGT_BASE/release
+
+mkdir -p $REL_TGT
+
+function header {
+	echo "# ==== $* ==== #"
+}
+
+cargo --version
+rustc --version
+
+pushd xde
+#
+# XXX This might be something that should be controlled by buildomat
+# itself through the config above. But for now I'll try to do it here.
+#
+# This is required for the unstable buid-std feature to work.
+#
+header "install rust-src"
+ptime -m rustup component add rust-src --toolchain nightly
+
+header "build xde (release)"
+ptime -m ./build.sh
+
+#
+# Inspect the kernel module for bad relocations in case the old
+# codegen issue ever shows its face again.
+#
+if elfdump $REL_SRC/xde | grep GOTPCREL; then
+	echo "found GOTPCREL relocation in release build"
+	exit 1
+fi
+popd
+
+pushd opteadm
+cargo +nightly build --release
+popd
+
+pushd pkg
+./build.sh
+
+banner copy
+pfexec mkdir -p /out
+pfexec chown "$UID" /out
+mv packages/repo/*.p5p /out/
---
+++ pkg/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PUBLISHER=helios-netdev
+export COMMIT_COUNT=`git rev-list --count HEAD`
+export REPO=packages/repo
+
+set -e
+set -x
+
+./clean.sh
+
+# create the proto area
+mkdir -p proto/kernel/drv/amd64
+mkdir -p proto/opt/oxide/opte/bin
+cp ../opteadm/target/release/opteadm proto/opt/oxide/opte/bin/
+cp ../xde/target/x86_64-unknown-unknown/release/xde proto/kernel/drv/amd64/xde
+
+# create the package
+sed -e "s/%PUBLISHER%/$PUBLISHER/g" \
+    -e "s/%COMMIT_COUNT%/$COMMIT_COUNT/g" \
+    opte.template.p5m | pkgmogrify -v -O opte.base.p5m
+
+pkgdepend generate -d proto opte.base.p5m > opte.generate.p5m
+
+mkdir -p packages
+pkgdepend resolve -d packages -s resolve.p5m opte.generate.p5m
+
+cat opte.base.p5m packages/opte.generate.p5m.resolve.p5m > opte.final.p5m
+
+pkgrepo create $REPO
+pkgrepo add-publisher -s $REPO $PUBLISHER
+
+pkgsend publish -d proto -s $REPO opte.final.p5m
+pkgrecv -a -d packages/repo/opte-0.1.$COMMIT_COUNT.p5p -s $REPO -v -m latest '*'
