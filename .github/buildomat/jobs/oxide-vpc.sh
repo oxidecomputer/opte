3ac196833b426ff8e168167168169d1e26e37e1c
Luqman Aden <luqman@oxide.computer>
2022-12-21 19:08:13 -0800

    opte/oxide-vpc: Use nightly when building with the `kernel` feature.

--- .github/buildomat/jobs/oxide-vpc.sh
+++ .github/buildomat/jobs/oxide-vpc.sh
@@ -28,14 +28,15 @@ header "check docs"
 # I believe this means any doc warnings in deps will cause this to
 # fail. Using a more targeted approach in the future might be nice.
 #
+# Use nightly which is needed for the `kernel` feature.
 RUSTDOCFLAGS="-D warnings" ptime -m \
-	    cargo doc --no-default-features --features=api,std,engine,kernel
+	    cargo +nightly doc --no-default-features --features=api,std,engine,kernel
 
 header "analyze std + api + usdt"
 ptime -m cargo check --features usdt
 
 header "analyze no_std + engine + kernel"
-ptime -m cargo check --no-default-features --features engine,kernel
+ptime -m cargo +nightly check --no-default-features --features engine,kernel
 
 header "test"
 ptime -m cargo test
