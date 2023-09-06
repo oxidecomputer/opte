063039304da968c8c8a1735eafa862779c9ed573
Kyle Simpson <kyle@oxide.computer>
2023-09-06 16:07:36 +0100

    Add some initial tools for performance graphing
    Mainly just using dtrace, these assume ou've started a workload
    elsewhere for now...

---
+++ dtrace/opte-count-cycles.d
@@ -0,0 +1,33 @@
+/*
+ * Build histograms of packet tx/rx time spent within XDE's Mac callbacks.
+ *
+ * dtrace -L ./lib -I . -Cqs ./opte-ht.d
+ */
+#include "common.h"
+#include "protos.d"
+
+#define	HDR_FMT "%-3s %-12s %-12s %-40s %-40s\n"
+
+xde_mc_tx:entry {
+	self->ts = vtimestamp;
+}
+
+xde_rx:entry {
+	self->ts = vtimestamp;
+}
+
+xde_mc_tx:return /self->ts/ {
+	@time["tx"] = lquantize((vtimestamp - self->ts), 1024, 65536, 1024);
+	self->ts = 0;
+}
+
+xde_rx:return /self->ts/ {
+	@time["rx"] = lquantize((vtimestamp - self->ts), 1024, 65536, 1024);
+	self->ts = 0;
+}
+
+tick-120s { exit(0); }
+
+END {
+
+}
