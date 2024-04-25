4cc823b50d3e4a629cdfaab2b3d3382514174ba9
Levon Tarver <11586085+internet-diglett@users.noreply.github.com>
2024-04-25 14:33:15 -0500

    add ability for omicron to observe v2p mappings (#494)
    We're adding rpws to Omicron so we can keep the v2p mappings consistent
    across sled reboots and other unfortunate-but-necessary events.
    Currently, sled-agent nor omicron has the ability to determine what
    mappings already exists, so it's difficult to reconcile or detect
    conflicting mappings.
    
    * enable dumping of v2p mappings via oxide-vpc
    * plumb through delete functionality for v2p mappings
    
    Related
    ---
    Closes https://github.com/oxidecomputer/opte/issues/332
    
    ---------
    
    Co-authored-by: oxide-renovate[bot] <146848827+oxide-renovate[bot]@users.noreply.github.com>

--- crates/opte-api/src/cmd.rs
+++ crates/opte-api/src/cmd.rs
@@ -35,6 +35,7 @@ pub enum OpteCmd {
     SetVirt2Boundary = 52,   // set a v2b mapping
     ClearVirt2Boundary = 53, // clear a v2b mapping
     DumpVirt2Boundary = 54,  // dump the v2b mappings
+    ClearVirt2Phys = 55,     // clear a v2p mapping
     AddRouterEntry = 60,     // add a router entry for IP dest
     CreateXde = 70,          // create a new xde device
     DeleteXde = 71,          // delete an xde device
@@ -62,6 +63,7 @@ impl TryFrom<c_int> for OpteCmd {
             52 => Ok(Self::SetVirt2Boundary),
             53 => Ok(Self::ClearVirt2Boundary),
             54 => Ok(Self::DumpVirt2Boundary),
+            55 => Ok(Self::ClearVirt2Phys),
             60 => Ok(Self::AddRouterEntry),
             70 => Ok(Self::CreateXde),
             71 => Ok(Self::DeleteXde),
