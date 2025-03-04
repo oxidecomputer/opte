d870573b5dd56d9a5158288dac2d25fe8d0c218c
Kyle Simpson <kyle@oxide.computer>
2025-03-04 12:01:47 +0000

    Rework rust toolchain installation
    CI on recent PRs is breaking, due to rustup 1.28.0+ no longer
    autoinstalling the correct rust toolchain version. This hurts us
    immediately since we have *two* toolchains (pinned nightly and stable),
    and deliberately specified the nightly for some tooling.
    
    This PR changes this over to use buildomat's auto-installation for the
    stable variant, and the new toolchain show -> install pattern for
    nightly. This also lets us place `$NIGHTLY` into most of our `cargo fmt`
    invocations, which should reduce the busywork in future compiler bumps
    for XDE.

--- .github/buildomat/jobs/opte-ioctl.sh
+++ .github/buildomat/jobs/opte-ioctl.sh
@@ -3,7 +3,7 @@
 #: name = "opte-ioctl"
 #: variety = "basic"
 #: target = "helios-2.0"
-#: rust_toolchain = "nightly-2024-11-18"
+#: rust_toolchain = true
 #: output_rules = []
 #:
 
@@ -11,9 +11,7 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
-function header {
-	echo "# ==== $* ==== #"
-}
+source .github/buildomat/common.sh
 
 cargo --version
 rustc --version
@@ -21,7 +19,7 @@ rustc --version
 cd lib/opte-ioctl
 
 header "check style"
-ptime -m cargo +nightly-2024-11-18 fmt -- --check
+ptime -m cargo +$NIGHTLY fmt -- --check
 
 header "analyze"
 ptime -m cargo clippy --all-targets
