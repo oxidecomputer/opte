b235e81ced9d220f6d8553e70e907f96ccb3efb9
Kyle Simpson <kyle@oxide.computer>
2025-05-30 18:21:16 +0100

    Prevent double-lookups on same port.

--- xde/src/mailbox.rs
+++ xde/src/mailbox.rs
@@ -7,6 +7,7 @@
 use crate::dev_map::FastKey;
 use alloc::collections::btree_map::BTreeMap;
 use core::mem;
+use core::ptr::NonNull;
 use opte::api::MacAddr;
 use opte::api::Vni;
 use opte::ddi::mblk::MsgBlk;
@@ -15,6 +16,8 @@ use opte::ddi::mblk::MsgBlkChain;
 /// Eh.
 pub struct Mailbox {
     boxen: BTreeMap<FastKey, MsgBlkChain>,
+    // Avoid any lookup on adjacent runs of packets hitting a single port.
+    last_caller: Option<(FastKey, NonNull<MsgBlkChain>)>,
 }
 
 impl Default for Mailbox {
@@ -25,7 +28,7 @@ impl Default for Mailbox {
 
 impl Mailbox {
     pub const fn new() -> Self {
-        Self { boxen: BTreeMap::new() }
+        Self { boxen: BTreeMap::new(), last_caller: None }
     }
 
     #[inline]
@@ -36,8 +39,27 @@ impl Mailbox {
 
     #[inline]
     pub fn post_by_key(&mut self, key: FastKey, pkt: MsgBlk) {
-        let chain =
-            self.boxen.entry(key).or_insert_with(|| MsgBlkChain::empty());
+        let chain = if let Some((stored_key, mut chain_ptr)) = self.last_caller
+            && stored_key == key
+        {
+            // SAFETY: We have a guarantee that the pointer is not aliased
+            // by holding `&mut self` -- this would invalidate any `&mut`s we
+            // might return.
+            // We know the pointer remains valid because:
+            // a) any inserts which could change the structure of `boxen`
+            //    unconditionally update this pointer after the insert is made
+            //    (they've used a different chain).
+            // b) chain pkt append will not add a new `MsgBlkChain` to `boxen`.
+            // c) `drain` (the only public way to remove entries from `boxen`)
+            //    sets `last_caller` to `None`.
+            unsafe { chain_ptr.as_mut() }
+        } else {
+            let chain =
+                self.boxen.entry(key).or_insert_with(|| MsgBlkChain::empty());
+            self.last_caller = Some((key, chain.into()));
+            chain
+        };
+
         chain.append(pkt);
     }
 
@@ -45,6 +67,7 @@ impl Mailbox {
     pub fn drain(&mut self) -> impl Iterator<Item = (FastKey, MsgBlkChain)> {
         let mut the_set = BTreeMap::new();
         mem::swap(&mut the_set, &mut self.boxen);
+        self.last_caller = None;
         the_set.into_iter()
     }
 }
