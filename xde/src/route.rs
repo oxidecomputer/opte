51f0620fd1a1efe0378827f81ee6d5610996c660
Kyle Simpson <kyle@oxide.computer>
2026-05-29 12:00:01 +0100

    Move `next_hop` out of the route cache write lock

--- xde/src/route.rs
+++ xde/src/route.rs
@@ -517,37 +517,46 @@ impl RouteCache {
     pub fn next_hop(&self, key: RouteKey, xde: &XdeDev) -> Route {
         let t = Moment::now();
 
-        let (maybe_route, map_ptr_int) = {
+        let (maybe_route, map_ptr_int, full) = {
             let route_cache = self.0.read();
             (
                 route_cache.get(&key).copied(),
                 &*route_cache as *const BTreeMap<_, _> as uintptr_t,
+                route_cache.len() >= MAX_CACHE_ENTRIES,
             )
         };
 
-        match maybe_route {
+        let probably_space_remaining = match maybe_route {
             Some(route) if route.is_valid(t) => {
                 route_hit_probe(map_ptr_int, &key);
                 return route.into();
             }
-            _ => {}
-        }
+            Some(_) => true,
+            _ => !full,
+        };
 
         // Cache miss: intent is to now ask illumos, then insert.
+        //
+        // This shouldn't duplicate work between threads, given that guests
+        // will place traffic from any flow onto the same tx queue.
+        //
+        // If someone *did* write in before us, we still have a valid route.
+        let route = match next_hop(&key, xde) {
+            Ok(route) => route,
+            Err(dev) => {
+                // `next_hop` might fail for myriad reasons, but we still
+                // send the packet on an underlay device depending on our
+                // progress. However, we do not want to cache bad mappings.
+                return Route::zero_addr(dev);
+            }
+        };
+
+        if !probably_space_remaining {
+            return route;
+        }
+
         let mut route_cache = self.0.write();
         let space_remaining = route_cache.len() < MAX_CACHE_ENTRIES;
-
-        // Someone else may have written while we were taking the lock.
-        // DO NOT waste time if there's a good route.
-        let maybe_route = route_cache.entry(key);
-        let entry_exists = match &maybe_route {
-            Entry::Occupied(e) if e.get().is_valid(t) => {
-                route_hit_probe(map_ptr_int, &key);
-                return (*e.get()).into();
-            }
-            Entry::Occupied(_) => true,
-            _ => false,
-        };
 
         // We've had a definitive flow miss, but we need to cap the cache
         // size to prevent excessive modification latencies at high flow
@@ -557,31 +566,20 @@ impl RouteCache {
         // XXX: Want to profile in future to see if LRU expiry is
         //      affordable/sane here.
         // XXX: A HashMap would exchange insert cost for lookup.
-        if entry_exists || space_remaining {
-            // `next_hop` might fail for myriad reasons, but we still
-            // send the packet on an underlay device depending on our
-            // progress. However, we do not want to cache bad mappings.
-            match (maybe_route, next_hop(&key, xde)) {
-                (Entry::Vacant(slot), Ok(route)) => {
-                    route_insert_probe(map_ptr_int, &key);
-                    slot.insert(route.cached(t));
-                    route
-                }
-                (Entry::Occupied(mut slot), Ok(route)) => {
-                    route_refresh_probe(map_ptr_int, &key);
-                    slot.insert(route.cached(t));
-                    route
-                }
-                (_, Err(dev)) => Route::zero_addr(dev),
+        match route_cache.entry(key) {
+            Entry::Occupied(mut slot) => {
+                route_refresh_probe(map_ptr_int, &key);
+                slot.insert(route.cached(t));
             }
-        } else {
-            route_full_probe(map_ptr_int, &key);
-            drop(route_cache);
-            match next_hop(&key, xde) {
-                Ok(route) => route,
-                Err(dev) => Route::zero_addr(dev),
+            Entry::Vacant(slot) if space_remaining => {
+                route_insert_probe(map_ptr_int, &key);
+                slot.insert(route.cached(t));
+            }
+            Entry::Vacant(_) => {
+                route_full_probe(map_ptr_int, &key);
             }
         }
+        route
     }
 
     /// Discards any cached route entries which have been present
