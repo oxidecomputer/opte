304ecfd4c07d7670bd67c4da70c5d89e21e0535e
Kyle Simpson <kyle@oxide.computer>
2026-09-03 13:45:45 +0100

    Review feedback

--- lib/opte/src/ddi/time.rs
+++ lib/opte/src/ddi/time.rs
@@ -6,7 +6,6 @@
 
 //! Moments, periodics, etc.
 use core::ops::Add;
-use core::ops::Sub;
 use core::time::Duration;
 
 cfg_if! {
@@ -59,20 +58,13 @@ impl Add<Duration> for Moment {
     }
 }
 
-impl Sub<Duration> for Moment {
+#[cfg(any(feature = "std", test))]
+impl core::ops::Sub<Duration> for Moment {
     type Output = Self;
 
     fn sub(self, rhs: Duration) -> Self::Output {
-        cfg_if! {
-            if #[cfg(all(not(feature = "std"), not(test)))] {
-                let new = self.inner - ((rhs.as_secs() * NANOS) as i64 +
-                    rhs.subsec_nanos() as i64);
-                Moment { inner: new }
-            } else {
-                let new = self.inner - rhs;
-                Moment { inner: new }
-            }
-        }
+        let new = self.inner.saturating_sub(rhs);
+        Moment { inner: new }
     }
 }
 
@@ -97,6 +89,16 @@ impl Moment {
             if #[cfg(all(not(feature = "std"), not(test)))] {
                 Self { inner: unsafe { ddi::gethrtime() } }
             } else {
+                // This is a pretty gross workaround for the lack of ways to get
+                // a raw numeric valus associated with an `Instant`. In order to
+                // enable `Self::raw()` and related functions on std, which allow
+                // us to manipulate flow timestamps using `Atomic` integers,
+                // we need to pick an arbitrary zero point and use our duration
+                // from there as the timestamp.
+                //
+                // Currently this is set a few minutes before program start, to
+                // enable benchmarks and tests which want one or more flows to
+                // begin in an expired state.
                 static FIRST_TS: OnceLock<Instant> = OnceLock::new();
 
                 let first_ts = *FIRST_TS.get_or_init(||
